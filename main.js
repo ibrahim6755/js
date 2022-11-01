71cbc73d2ca0319e50f8182447ed62540b37a22d
ibrahim6755 <ibrahimghani020@gmail.com>
2022-11-01 18:00:43 +0000

    calculator

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-function one(){
-    alert('Learning Java Script')
+function add(){
+  let n1 = document.getElementById('txt-1').value;
+  let n2 = document.getElementById('txt-2').value;
+  let r = parseInt(n1) + parseInt(n2);
+  let n3 = document.getElementById('txt-3').value = r;
+}
+function subtract(){
+    let n1 = document.getElementById('txt-1').value;
+    let n2 = document.getElementById('txt-2').value;
+    let r = parseInt(n1) - parseInt(n2);
+    let n3 = document.getElementById('txt-3').value = r;
+}
+function multiply(){
+    let n1 = document.getElementById('txt-1').value;
+    let n2 = document.getElementById('txt-2').value;
+    let r = parseInt(n1) * parseInt(n2);
+    let n3 = document.getElementById('txt-3').value = r;
+}
+function division(){
+    let n1 = document.getElementById('txt-1').value;
+    let n2 = document.getElementById('txt-2').value;
+    let r = parseInt(n1) / parseInt(n2);
+    let n3 = document.getElementById('txt-3').value = r;
 }
